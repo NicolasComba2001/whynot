df9b9feb6647c5fe7309cecdded0df9923dd5f82
nc <nc@com.ar>
2023-08-10 22:26:51 -0300

    creando la pagina inicial, solo interfaz

---
+++ src/assets/components/front-page/FrontPage.tsx
@@ -0,0 +1,36 @@
+import frontPageImg from '../../all-images/image-of-frontPage/frontPageImg.jpg';
+
+
+export function FrontPage(){
+    return(
+        <section>
+
+            {/* menu */}
+
+            <section className='grid grid-cols-autoFit md:place-items-none place-items-center gap-8 h-screen  w-[90%] m-auto'>
+
+                <section className='h-[65%] w-full'>
+                    <article>
+                        <p className='text-8xl leading-normal'>CHANGE YOUR MIND</p>
+                        <p className='text-8xl leading-normal'>LOVE YOURSELF</p>
+                        <p className='text-8xl leading-normal'>CHOOSE WELL</p>
+                        <p className='text-8xl leading-normal underline decoration-orange-500'>BUY NOW!</p>
+                    </article>
+                    <button className='bg-[#ceb89e] mt-8 p-4 rounded-md text-base normal-family-font float-right'>Conoce nuestra marca</button>
+                </section>
+                <figure style={{
+                    backgroundImage: `url(${frontPageImg})`,
+                    backgroundPosition: "right",
+                    backgroundRepeat: "no-repeat",
+                    width: "100%",
+                    height: "100%",
+                    backgroundSize: 'contain'
+                }}>
+                    {/* <img src={frontPageImg} className='w-full h-full' alt="hombre modelando una remera, negra lisa." /> */}
+                </figure>
+
+            </section>
+
+        </section>
+    )
+}
